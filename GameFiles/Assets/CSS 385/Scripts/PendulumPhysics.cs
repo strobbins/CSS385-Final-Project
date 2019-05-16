9f89c3c8779cb0780940895a7a9b628f9359dd63
rtustison <rachaeltustison@gmail.com>
2019-05-16 06:59:08 -0700

    Rachael's push and addition of a pendulum physics object
    Created a pendulum script that is WIP, will swing left and right, but eventually will flip around to match the gravity of the player.

---
+++ GameFiles/Assets/CSS 385/Scripts/PendulumPhysics.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PendulumPhysics : MonoBehaviour
+{
+    public RigidBody2D body2d;
+    public float leftPush;
+    public float rightPush;
+    public float velocityThreshold;
+
+    void Start()
+    {
+        body2d = GetComponent(RigidBody2D());
+        body2d.angularVelocity = velocityThreshold;
+    }
+
+    void Update()
+    {
+        Push();
+    }
+
+    public void Push()
+    {
+        if(transform.rotation.z> 0
+            && (body2d.angularVelocity.z < rightPush)
+            && (body2d.angularvelocity > 0)
+            && body2d.angularVelocity < velocityThreshold)
+        {
+            body2d.angularVelocity = velocityThreshold;
+        } else if(transform.rotation.z < 0
+            && (body2d.angularVelocity > leftPush)
+            && (body2d.angularVelocity < 0)
+            && body2d.angularVelocity > velocityThreshold * -1) {
+            body2d.angularVelocity = velocityThreshold * -1;
+        }
+    }
+}
