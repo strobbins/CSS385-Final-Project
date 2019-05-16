377b4831ab7d18d0d9602cfbf99e345b4a413569
Dakota Sagredo <dakota_sagredo@hotmail.com>
2019-05-16 13:27:14 -0700

    Added respawn point, and example of moving box

---
+++ GameFiles/Assets/CSS 385/Scripts/Deathzone.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deathzone : MonoBehaviour {
+
+	// SerializedField creates a data field to appear on Unity, while still
+	// keeping the variable private, so it can't be accessed elsewhere
+	[SerializeField] private Transform player;
+	[SerializeField] private Transform respawnPoint;
+
+	// When player touches collider, teleport player to respawn point
+	void OnTriggerEnter2D(Collider2D col) {
+		player.transform.position = respawnPoint.transform.position;
+	}
+}
